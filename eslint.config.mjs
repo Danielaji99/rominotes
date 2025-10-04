c8f885711ebd1fec5d2cdf4a834d86c6bf438823
Winner Edwin <winneredwin2667@gmail.com>
2025-10-04 15:09:39 +0100

    Setup complete

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import tseslint from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -20,6 +21,13 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  ...tseslint.configs.recommended,
+   {
+     rules: {
+       "no-console": ["warn", { allow: ["error"] }],
+       "no-restricted-syntax": "error",
+     },
+   },
 ];
 
 export default eslintConfig;
---
+++ lint-staged.config.js
@@ -0,0 +1,7 @@
+/** @type {import("lint-staged").Configuration} */
+const config = {
+  "*.{js,jsx,ts,tsx}": [() => "tsc -b", "eslint --fix --no-warn-ignored"],
+  "*.{js,jsx,ts,tsx,md,html,css}": "prettier --write",
+};
+
+export default config;
